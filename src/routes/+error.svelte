c34590e805b596a2fe11476ce4471de6bd81024e
Sean Collings <seanrcollings@gmail.com>
2023-04-08 15:16:25 -0600

    setup error page & some dependencies

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<main>
+	<h1 class="text-5xl">Whoops, something has gone wrong</h1>
+
+	<p>
+		<a href="/">Go back to the homepage</a>
+	</p>
+
+	<p>Or play some Snake to take your mind off things</p>
+
+	<iframe src="https://seanrcollings.github.io/wasm-snake/" frameborder="0" title="Snake" />
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
+	}
+
+	iframe {
+		width: 100%;
+		height: 770px;
+	}
+</style>
