82ce424fcb6c05e9a7afa288aa1794c518b02ad1
Jaren Glenn <jarenglenn@gmail.com>
2023-04-25 11:37:14 -0600

    log errors in scraper

--- src/lib/server/worker/worker.ts
+++ src/lib/server/worker/worker.ts
@@ -19,23 +19,28 @@ new Worker<ScrapeData>(
 	'Scrape',
 	async (job) => {
 		const { url, uid, listId, itemId } = job.data;
-		const scraper = new Scraper(url);
-		const data = await scraper.scrape();
+		try {
+			const scraper = new Scraper(url);
+			const data = await scraper.scrape();
 
-		console.log(`Scraped ${url} for /user/${uid}/list/${listId}/item/${itemId}`);
+			console.log(`Scraped ${url} for /user/${uid}/list/${listId}/item/${itemId}`);
 
-		const item = await itemsClient.show(uid, listId, itemId);
-		if (!item) return;
+			const item = await itemsClient.show(uid, listId, itemId);
+			if (!item) return;
 
-		const updated = {
-			title: item.title || data.title,
-			description: item.description || data.description,
-			image: item.image || data.image,
-			seller: item.seller || data.seller,
-			price: item.price || data.price
-		};
+			const updated = {
+				title: item.title || data.title,
+				description: item.description || data.description,
+				image: item.image || data.image,
+				seller: item.seller || data.seller,
+				price: item.price || data.price
+			};
 
-		itemsClient.update([uid, listId, itemId], updated);
+			itemsClient.update([uid, listId, itemId], updated);
+		} catch (exc) {
+			console.error(exc);
+			throw exc;
+		}
 	},
 	{ connection, concurrency: 1 }
 );
